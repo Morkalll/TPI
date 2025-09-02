10d5224d4e1e37a93f8fef544f2c9c950835215a
matufl <floresmateo297@gmil.com>
2025-09-02 11:31:52 -0300

    Desarrollo de MovieDetail y CandyDetail

--- src/components/pages/MovieDetail/MovieDetail.jsx
+++ src/components/pages/MovieDetail/MovieDetail.jsx
@@ -1,20 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { NavBar } from '../../NavBar/NavBar';
+import { MoviesMock } from "../../../data/MoviesMock";
+import './MovieDetail.css';
 
+export const MovieDetail = () => {
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
 
-export function MovieDetail(){
-    const { id } = useParams();
-    const movie = movie.find((m) => m.id.toString() === id);
+  useEffect(() => {
+    const foundMovie = MoviesMock.find(item => item.id === parseInt(id));
+    setMovie(foundMovie);
+  }, [id]);
 
-    if (!movie) return <h2>Pelicula no encontrada :/</h2>;
+  if (!movie) return <h2>Película no encontrada :/</h2>;
 
-    return(
-        <div>
-            <h1>{movie.title}</h1>
-            <p>Género: {movie.genre}</p>
-            <p>Duración: {movie.duration}</p>
-            <p>Calificación: {movie.rating}</p>
-            <p>{movie.synopsis}</p>
-            <img src = {movie.poster} alt = {movie.title} width = "100px" />
+  return (
+    <div>
+      <NavBar />
+      <div className="movie-detail-container">
+        <h2>{movie.title}</h2>
+        <img src={movie.poster} alt={movie.title} />
+        <div className="movie-info">
+          <p><strong>Género:</strong> {movie.genre}</p>
+          <p><strong>Duración:</strong> {movie.duration}</p>
+          <p><strong>Calificación:</strong> {movie.rating}</p>
+          <p><strong>Sinopsis:</strong> {movie.synopsis}</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
