1ee8a574efda68891a73dc355017ba45d7c68c79
Morkalll <sebastiandiazdarth@gmail.com>
2025-10-26 13:28:35 -0300

    Borrado de carpeta duplicada

--- TPI-main/server/src/config.js
+++
@@ -1,5 +0,0 @@
-
-export const PORT = process.env.PORT || 3000;
-export const JWT_SECRET = process.env.JWT_SECRET || "programacion3-2025";
-export const DB_PATH = process.env.DB_PATH || "movies.db";
-
